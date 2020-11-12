9cc6d99d81535c78588562484ecaf32110c3810b
Loic Olinga <olingaloic@gmail.com>
2020-11-12 18:53:27 -0500

    first commit

---
+++ frontend/build/precache-manifest.5644b5e55e58c3e13df8d5a43509a91f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5696e26e96f2320c86e031c330811c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5d0d6b002414f6f7be2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d768440cd74f55a96bdc",
+    "url": "/static/css/main.34740ae9.chunk.css"
+  },
+  {
+    "revision": "a5d0d6b002414f6f7be2",
+    "url": "/static/js/2.5f47e2e9.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.5f47e2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d768440cd74f55a96bdc",
+    "url": "/static/js/main.dbe6d76d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
