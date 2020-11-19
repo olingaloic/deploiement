cf196a4e40ee83619340177a1524f2c0a16f8233
Loic Olinga <olingaloic@gmail.com>
2020-11-19 15:48:31 -0500

    changement du lien dans l'annotation cross origin dans les controlleurs de l'applications spring boot

--- src/main/java/com/equipe1/controller/EmployeurController.java
+++ src/main/java/com/equipe1/controller/EmployeurController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
-//@CrossOrigin(origins = "http://reactveilledeploiementaws.s3-website-us-east-1.amazonaws.com")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://react-veille-deploiement.uk.r.appspot.com")
 @RestController
 @RequestMapping("/employeurs")
 public class EmployeurController {
