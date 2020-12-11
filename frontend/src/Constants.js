46eca8a9c9d00c2250f56a7394b99ccf2afc49c7
Richard Truong <richard.truong084@gmail.com>
2020-12-10 21:39:27 -0500

    fix erreur build Auth-Service.js références Constants.js

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,8 +1,8 @@
 // For Best Practices https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
 
 // Azure
-//export const API_URL = 'http://localhost:8080'
-export const API_URL = 'https://azure-veille-technologique.azurewebsites.net'
+export const API_URL = 'http://localhost:8080'
+//export const API_URL = 'https://azure-veille-technologique.azurewebsites.net'
 
 // AWS
 //export const API_URL = 'http://localhost:5000'
--- frontend/src/service/security/auth.service.js
+++ frontend/src/service/security/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_URL } from '../Constants';
+import { API_URL } from '../../Constants';
 
 const URL = API_URL + "/api/auth/";
 
